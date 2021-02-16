49b7414bab99fe8584f6fb5689ad2c3a301e5658
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2021-02-16 03:47:00 -0300

    Criação dos modelos que serão utilizados no projeto

---
+++ bhs/Models/Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bhs.Models
+{
+    public class Sale
+    {
+        public enum SaleStatus
+        {
+            WaitingPayment,
+            PaymentApproved,
+            InTransit,
+            Delivered,
+            Canceled
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public SaleStatus Status { get; set; }
+
+        [Required]
+        public Saler Saler { get; set; }
+
+        [Required]
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        
+    }
+}
---
+++ bhs/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bhs.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Code { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int YearOfManufacture { get; set; }
+    }
+}
